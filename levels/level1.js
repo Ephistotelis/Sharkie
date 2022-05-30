23afabc86307921812d94bcb049f30440648ce7f
Joshua Herrmann <joshua.herrmann1@web.de>
2022-05-30 21:01:18 +0200

    changed background to avoid grafic bugs, gonna be fixed at the end

--- levels/level1.js
+++ levels/level1.js
@@ -7,7 +7,10 @@ const level1 = new Level(
     ], [
         new Barrier()
     ], [
-        new Background('img/3. Background/Layers/5. Water/D1.png', 0),
+        // fürs erste ohne moving background, später mit moving background und die grafix bugs fixen
+        new Background('img/3. Background/Dark/1.png', 0),
+        new Background('img/3. Background/Dark/2.png', 719),
+        /* new Background('img/3. Background/Layers/5. Water/D1.png', 0),
         new Background('img/3. Background/Layers/4.Fondo 2/D1.png', 0),
         new Background('img/3. Background/Layers/1. Light/1.png', 0),
         new Background('img/3. Background/Layers/3.Fondo 1/D1.png', 0),
@@ -17,6 +20,6 @@ const level1 = new Level(
         new Background('img/3. Background/Layers/4.Fondo 2/D2.png', 719),
         new Background('img/3. Background/Layers/3.Fondo 1/D2.png', 719),
         new Background('img/3. Background/Layers/1. Light/2.png', 719),
-        new Background('img/3. Background/Layers/2. Floor/D2.png', 719),
+        new Background('img/3. Background/Layers/2. Floor/D2.png', 719), */
     ]
 );
